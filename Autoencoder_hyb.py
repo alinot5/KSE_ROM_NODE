dea88d927cfad1008368dcd4ac72bde6c6ac7de2
alinot5 <46662557+alinot5@users.noreply.github.com>
2022-06-01 12:58:35 -0500

    Add files via upload

---
+++ Autoencoder_hyb.py
@@ -0,0 +1,174 @@
+#! /home/linot/anaconda3/bin/python3
+"""
+Created on Wed Aug 14 2019
+
+Dense invariant NN with tied encoder and decoder
+
+@author: Alec
+"""
+
+import os
+import sys
+import math
+
+import numpy as np
+import pickle
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import tensorflow as tf
+from tensorflow import keras
+import tensorflow.keras.backend as K
+
+import scipy.io
+from sklearn.utils.extmath import randomized_svd
+import buildmodel
+import time
+
+# Class for printing history
+class PrintDot(keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs):
+      newfile=open('OutHyb.txt','a+')
+      newfile.write('Epoch: '+str(epoch)+'   ')
+      newfile.write('Loss: '+str(logs['loss'])+'\n')
+      newfile.close()
+
+###############################################################################
+# Plotting Functions
+###############################################################################
+# For plotting history
+def plot_history(histshift):
+    plt.figure()
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.semilogy(histshift['epoch'], histshift['loss'],
+       label='Train Loss')
+    plt.semilogy(histshift['epoch'], histshift['val_loss'],
+       label = 'Val Loss')
+    plt.legend()
+    #plt.ylim([10**-6,10**-1])
+
+
+def plot_parity(utt_test,test_predictions,KSmax=3.5):
+    plt.plot(utt_test.flatten(), test_predictions.flatten(),'o',color='black',markersize=.5)
+    plt.xlabel('True Values')
+    plt.ylabel('Predictions')
+    plt.axis('equal')
+    plt.axis('square')
+    plt.xlim([-KSmax,KSmax])
+    plt.ylim([-KSmax,KSmax])
+    plt.plot([-100, 100], [-100, 100])
+
+def plot_hist(error,title,bins=40,ymax=50,xmax=.05):
+    plt.hist(error, bins = bins,density=True)
+    plt.xlim([-xmax,xmax])
+    plt.ylim([0,ymax])
+    plt.xlabel("Prediction Error")
+    variance=np.mean(error**2)
+    plt.title('MSE='+str(round(variance,7)))
+    plt.ylabel(title+' PDF')
+    
+    return variance
+
+def scheduler(epoch, lr):
+    if epoch < 500:
+        return .001
+    else:
+        return .0001
+
+###############################################################################
+# Main Function
+###############################################################################
+if __name__=='__main__':
+    # Load data
+    u_train=pickle.load(open('Data_Train.p','rb'))
+    u_test=pickle.load(open('Data_Test.p','rb'))
+    [M,N]=u_train.shape
+
+    u_mean=np.mean(u_train,axis=0)
+    u_std=np.std(u_train,axis=0)
+    u_train=(u_train-u_mean[np.newaxis,:])/u_std[np.newaxis,:]
+    u_test=(u_test-u_mean[np.newaxis,:])/u_std[np.newaxis,:]
+
+    # Perform SVD on the training data
+    U,S,VT=randomized_svd(u_train.transpose(), n_components=N)
+    pickle.dump([u_mean,u_std,U],open('PCA.p','wb'))
+
+    # Store errors for figure
+    MSEsig=[]
+    MSEpca=[]
+
+    # For timing training
+    timer=time.time()
+    # Loop over latent dimensions
+    for trunc in range(5,11):
+        ###########################################################################
+        # Autoencoder
+        ###########################################################################
+        # Build model
+        model,EPOCHS,optimizer=buildmodel.buildmodel_hybrid(N,U,trunc)
+
+        # Compile model
+        model.compile(loss='mse',optimizer=optimizer,metrics=['mae', 'mse'])
+
+        print(model.summary())
+        
+        # Train the model 
+        callback = tf.keras.callbacks.LearningRateScheduler(scheduler)
+        history = model.fit(u_train, [u_train,np.zeros((len(u_train),trunc))],epochs=EPOCHS, validation_data=(u_test[:1000:10,:],[u_test[:1000:10,:],np.zeros((len(u_test[:1000:10,:]),trunc))]), verbose=0,callbacks=[PrintDot(),callback])
+
+        # Save history for comparing models
+        hist = pd.DataFrame(history.history)
+        hist['epoch'] = history.epoch
+
+        ###########################################################################
+        # Plot Results
+        ###########################################################################
+        
+        # Plot MAE and MSE vs epochs
+        plot_history(hist)
+        plt.tight_layout()
+        plt.savefig(open('Error'+str(trunc)+'Hyb.png','wb'))
+        plt.close()
+
+        [pred_u,_]=model.predict(u_test)    
+        # Real Space PCA prediction (Fourier Modes)
+        PCA_u=u_test@ U[:,:trunc]@ U[:,:trunc].transpose()
+
+        # Plot side by side histograms
+        plt.figure()
+        # Plot side by side histograms
+        plt.subplot(1,2,1)
+        error = u_test.flatten() - pred_u.flatten()
+        MSE=plot_hist(error,'NN')
+        plt.subplot(1,2,2)
+        error = u_test.flatten() - PCA_u.flatten()
+        MSE_PCA=plot_hist(error,'PCA')
+        plt.tight_layout()
+        plt.savefig(open('Statistics'+str(trunc)+'Hyb.png','wb'))
+        plt.close()
+        
+        # Save models
+        model.save_weights('model'+str(trunc)+'Hyb.h5')
+
+        # Save MSE
+        pickle.dump([MSE,MSE_PCA],open('MSE'+str(trunc)+'Hyb.p','wb'))
+        MSEsig.append(MSE)
+        MSEpca.append(MSE_PCA)
+    
+    # Output the time it took to train
+    newfile=open('OutHyb.txt','a+')
+    newfile.write('Time: '+str(time.time()-timer))
+    newfile.close()
+
+    # Plot the Errors for comparison
+    HL=np.arange(5,11)
+    plt.figure()
+    plt.semilogy(HL, MSEsig,'x',label='Sig')
+    plt.semilogy(HL,MSEpca,'s',label='PCA')
+    plt.legend()
+    plt.xlabel(r'$d_h$')
+    plt.ylabel('MSE')
+    plt.savefig(open('ComparisonHyb.png','wb'))
---
+++ buildmodel.py
@@ -0,0 +1,247 @@
+#! /opt/cluster_soft/anaconda3/bin/python3
+"""
+Created on Thu Mar 28 13:28:41 2019
+
+This is the architecture of the NN used in all of the trials in this folder
+
+@author: Alec
+"""
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Embedding, LSTM, Dense,Lambda
+import tensorflow.keras.backend as K
+from functools import reduce
+import os
+
+#Note: All operations must go into Lambda layers
+# Dense layer equivariance in main function
+def buildmodel(N,trunc):
+    K.set_floatx('float64')
+    #Parameters
+    optimizer = tf.keras.optimizers.Adam()
+    EPOCHS=1000
+    hiddenin=[500,trunc]
+    actin=['sigmoid',None]
+    hiddenout=[500,N]
+    actout=['sigmoid',None]
+
+    # Input size
+    main_input = layers.Input(shape=(N,), name='main_input')
+
+    ########################################################################################
+    # Encoder
+    ########################################################################################
+    # Nonlinear dim increase
+    encode=main_input
+    for i in range(len(hiddenin)):
+        encode=layers.Dense(hiddenin[i],activation=actin[i],name='Dense_In'+str(i+1))(encode)
+
+    hidden=encode
+
+    ########################################################################################
+    # Decoder
+    ########################################################################################
+    # Nonlinear dim increase
+    decode=hidden
+    for i in range(len(hiddenout)):
+        decode=layers.Dense(hiddenout[i],activation=actout[i],name='Dense_Out'+str(i+1))(decode)
+
+    main_output=decode
+
+    # Build model
+    model=Model(inputs=main_input,outputs=main_output)
+    
+    return model,EPOCHS,optimizer
+
+# Dense layer equivariance in main function
+def encode(N):
+    K.set_floatx('float64')
+    #Parameters
+    hiddenin=[500,trunc]
+    actin=['relu',None]
+
+    # Input size
+    main_input = layers.Input(shape=(N,), name='main_input')
+
+    ########################################################################################
+    # Encoder
+    ########################################################################################
+    # Nonlinear dim increase
+    encode=main_input
+    for i in range(len(hiddenin)):
+        encode=layers.Dense(hiddenin[i],activation=actin[i],name='Dense_In'+str(i+1))(encode)
+
+    main_output=encode
+
+    # Build model
+    model=Model(inputs=main_input,outputs=main_output)
+    
+    return model
+
+# Dense layer equivariance in main function
+def decode(N):
+    K.set_floatx('float64')
+    #Parameters
+    hiddenout=[500,N]
+    actout=['relu',None]
+
+    # Input size
+    main_input = layers.Input(shape=(trunc,), name='main_input')
+
+    ########################################################################################
+    # Decoder
+    ########################################################################################
+    # Nonlinear dim increase
+    decode=main_input
+    for i in range(len(hiddenout)):
+        decode=layers.Dense(hiddenout[i],activation=actout[i],name='Dense_Out'+str(i+1))(decode)
+
+    main_output=decode
+
+    # Build model
+    model=Model(inputs=main_input,outputs=main_output)
+    
+    return model
+    
+def buildmodel_hybrid(N,U,trunc):
+    K.set_floatx('float64')
+    #Parameters
+    optimizer = tf.keras.optimizers.Adam()
+    EPOCHS=5000
+    Utens=K.variable(value=U)
+
+    hiddenin=[500,trunc]
+    actin=['sigmoid',None]
+
+    hiddenout_PCA=[500,trunc]
+    hiddenout=[500,N-trunc]
+    actout=['sigmoid',None]
+
+    ########################################################################################
+    # Encoder
+    ########################################################################################
+    # Input size
+    main_input = layers.Input(shape=(N,), name='main_input')
+
+    # Change of basis
+    PCA_input=Lambda(lambda x: tf.einsum("ij,jk->ik",x,Utens), name='PCA_input')(main_input)
+
+    # Nonlinear dim reduction
+    encode=PCA_input
+    for i in range(len(hiddenin)):
+        encode=layers.Dense(hiddenin[i],activation=actin[i],name='Dense_In'+str(i+1))(encode)
+
+    # Add linear and nonlinear dim reduction
+    PCA_input_trunc=Lambda(lambda x: tf.slice(x,[0,0],[-1,trunc]))(PCA_input)
+    hidden=layers.Add(name='Hidden_Layer')([encode, PCA_input_trunc])
+
+    ########################################################################################
+    # Decoder
+    ########################################################################################
+    # Linear dim increase
+    paddings=tf.constant([[0,0,],[0,N-trunc]])
+    PCA_output=Lambda(lambda x: tf.pad(x,paddings,'CONSTANT'), name='PCA_Int_Output')(hidden)
+
+    # Nonlinear dim increase
+    decode=hidden
+    for i in range(len(hiddenout)):
+        decode=layers.Dense(hiddenout[i],activation=actout[i],name='Dense_Out'+str(i+1))(decode)
+
+    #Append tied layers on top
+    conc=hidden
+    for i in range(len(hiddenout_PCA)):
+        conc=layers.Dense(hiddenout_PCA[i],activation=actout[i],name='Dense_Out_Reg'+str(i+1))(conc)
+    # This layer penalizes when the top terms of the encoder do not equal those of the decoder
+    reg=layers.Add(name='Regularize')([conc,encode])
+    # This combines both the top truncated values with the remaining values
+    decode=layers.Concatenate(name='Concatenate')([conc,decode])
+
+    # Add PCA results to NN results
+    PCA_output=layers.Add(name='PCA_Output')([decode, PCA_output])
+
+    # Change of basis back into the full space
+    main_output=Lambda(lambda x: tf.einsum("ij,kj->ik",x,Utens), name='Output')(PCA_output)
+
+    # Build model
+    model=Model(inputs=main_input,outputs=[main_output,reg])
+
+    return model,EPOCHS,optimizer
+
+def encode_hybrid(N,U,trunc):
+    K.set_floatx('float64')
+    #Parameters
+    Utens=K.variable(value=U)
+
+    hiddenin=[500,trunc]
+    actin=['sigmoid',None]
+
+    ########################################################################################
+    # Encoder
+    ########################################################################################
+    # Input size
+    main_input = layers.Input(shape=(N,), name='main_input')
+
+    # Change of basis
+    PCA_input=Lambda(lambda x: tf.einsum("ij,jk->ik",x,Utens), name='PCA_input')(main_input)
+
+    # Nonlinear dim reduction
+    encode=PCA_input
+    for i in range(len(hiddenin)):
+        encode=layers.Dense(hiddenin[i],activation=actin[i],name='Dense_In'+str(i+1))(encode)
+
+    # Add linear and nonlinear dim reduction
+    PCA_input_trunc=Lambda(lambda x: tf.slice(x,[0,0],[-1,trunc]))(PCA_input)
+    hidden=layers.Add(name='Hidden_Layer')([encode, PCA_input_trunc])
+
+    # Build model
+    model=Model(inputs=main_input,outputs=hidden)
+
+    return model
+
+def decode_hybrid(N,U,trunc):
+    K.set_floatx('float64')
+    #Parameters
+    Utens=K.variable(value=U)
+
+    hiddenout_PCA=[500,trunc]
+    hiddenout=[500,N-trunc]
+    actout=['sigmoid',None]
+
+    ########################################################################################
+    # Encoder
+    ########################################################################################
+    # Input size
+    hidden = layers.Input(shape=(trunc,), name='main_input')
+
+    ########################################################################################
+    # Decoder
+    ########################################################################################
+    # Linear dim increase
+    paddings=tf.constant([[0,0,],[0,N-trunc]])
+    PCA_output=Lambda(lambda x: tf.pad(x,paddings,'CONSTANT'), name='PCA_Int_Output')(hidden)
+
+    # Nonlinear dim increase
+    decode=hidden
+    for i in range(len(hiddenout)):
+        decode=layers.Dense(hiddenout[i],activation=actout[i],name='Dense_Out'+str(i+1))(decode)
+
+    #Append tied layers on top
+    conc=hidden
+    for i in range(len(hiddenout_PCA)):
+        conc=layers.Dense(hiddenout_PCA[i],activation=actout[i],name='Dense_Out_Reg'+str(i+1))(conc)
+
+    # This combines both the top truncated values with the remaining values
+    decode=layers.Concatenate(name='Concatenate')([conc,decode])
+
+    # Add PCA results to NN results
+    PCA_output=layers.Add(name='PCA_Output')([decode, PCA_output])
+
+    # Change of basis back into the full space
+    main_output=Lambda(lambda x: tf.einsum("ij,kj->ik",x,Utens), name='Output')(PCA_output)
+
+    # Build model
+    model=Model(inputs=hidden,outputs=main_output)
+
+    return model
